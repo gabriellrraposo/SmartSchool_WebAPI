1dcc363ef8e3dc6076b444513c8bdcc2715f9a41
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-22 18:00:44 -0300

    Initial Commit

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSchool_WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlunoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello Aluno");
+        }
+    }
+}
---
+++ Controllers/ProfessorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSchool_WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfessorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello Professor");
+        }
+    }
+}
